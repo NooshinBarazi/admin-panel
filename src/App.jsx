780380d73de7489f65aa67aeb921a25c9c04dd3a
nooshinbarazi <nooshinbry@gmail.com>
2023-11-22 20:57:08 +0330

    translate the login page and redirect to the courses page

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,24 @@
 import { RouterProvider } from "react-router-dom";
-import router from './router';
-import './core/i18n'
+import router from "./router";
+import "./core/i18n";
 import { useAppContext } from "./contexts/app/app-context";
-import { useEffect } from "react";
-
-
+import React, { useEffect } from "react";
 
 function App() {
-  const {theme} = useAppContext();
-  useEffect(()=>{
+  const { theme } = useAppContext();
+  useEffect(() => {
     const head = document.head;
-    const link = document.createElement('link');
-    link.rel = 'stylesheet';
+    const link = document.createElement("link");
+    link.rel = "stylesheet";
     link.href = `/css/${theme}.css`;
     head.appendChild(link);
 
-    return()=>{
+    return () => {
       head.removeChild(link);
-    }
-  }, [theme])
-  return <RouterProvider router={router}/>;
+    };
+  }, [theme]);
+
+  return <RouterProvider router={router} />;
 }
 
 export default App;
