90fa1cee688c6ce8c269ac748a04037aa53c8b67
nooshinbarazi <nooshinbry@gmail.com>
2023-11-30 22:44:59 +0330

    define the interceptors

--- src/core/http-service.js
+++ src/core/http-service.js
@@ -1,7 +1,22 @@
 import axios from "axios";
 
-const BASE_URL = 'https://react-mini-projects-api.classbon.com';
+const BASE_URL = "https://react-mini-projects-api.classbon.com";
 
 export const httpService = axios.create({
-    baseURL: BASE_URL,   
-})
+  baseURL: BASE_URL,
+});
+
+const httpInterseptedService = axios.create({
+  baseURL: BASE_URL,
+});
+
+httpInterseptedService.interceptors.request.use(async (config) => {
+  const token = localStorage.getItem("token");
+  if (token) {
+    config.headers = {
+      authorization: `Bearer ${token}`,
+    };
+    return config;
+  };
+  (error) => Promise.reject(error);
+});
