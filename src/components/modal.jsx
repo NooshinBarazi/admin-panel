81075ecbe28ebda436ac9d491a5f7206de229e29
nooshinbarazi <nooshinbry@gmail.com>
2023-12-18 18:30:33 +0330

    build modal component for delete button

--- src/components/modal.jsx
+++ src/components/modal.jsx
@@ -1,11 +1,39 @@
 import { createPortal } from "react-dom";
 
-const Modal = ({isOpen, open, title, body, children}) => {
-    return (
-        <>
-        {isOpen && createPortal(<div>show modal</div>, document.getElementById('modal'))}
-        </>
-    );
-}
- 
+const Modal = ({ isOpen, setShowModalDelete, title, body, children }) => {
+  return (
+    <>
+      {isOpen &&
+        createPortal(
+          <div
+            className="modal"
+            style={{ display: "block" }}
+            onClick={() => setShowModalDelete(false)}
+          >
+            <div
+              className="modal-dialog modal-dialog-centered"
+              onClick={(e) => e.stopPropagation()}
+            >
+              <div className="modal-content">
+                <div className="modal-header">
+                  <h5 className="modal-title fw-bolder">{title}</h5>
+                  <button
+                    type="button"
+                    className="btn-close m-0"
+                    onClick={() => setShowModalDelete(false)}
+                  ></button>
+                </div>
+                <div className="modal-body m-3">
+                  <p className="mb-0">{body}</p>
+                </div>
+                <div className="modal-footer">{children}</div>
+              </div>
+            </div>
+          </div>,
+          document.getElementById("modal")
+        )}
+    </>
+  );
+};
+
 export default Modal;
--- src/pages/course-categories.jsx
+++ src/pages/course-categories.jsx
@@ -1,11 +1,14 @@
 import { Await, defer, useLoaderData } from "react-router-dom";
 import { httpInterseptedService } from "../core/http-service";
 import CategoryList from "../features/categories/components/category-list";
-import { Suspense } from "react";
+import { Suspense, useState } from "react";
+import Modal from "../components/modal";
 
 const CourseCategories = () => {
   const data = useLoaderData();
+  const [showModalDelete, setShowModalDelete] = useState(false)
   return (
+    <>
     <div className="row">
       <div className="col-12">
       <div className="d-flex align-items-center justify-content-between mb-5">
@@ -16,11 +19,16 @@ const CourseCategories = () => {
         </div>
         <Suspense fallback = {<p className="text-info"> در حال دریافت اطلاعات ...</p>}>
           <Await resolve={data.categories}>
-            {(loadedCategories) => <CategoryList categories={loadedCategories} />}
+            {(loadedCategories) => <CategoryList categories={loadedCategories} setShowModalDelete={setShowModalDelete} />}
           </Await>
         </Suspense>
       </div>
     </div>
+    <Modal isOpen={showModalDelete} setShowModalDelete={setShowModalDelete} title='حذف' body='آیا از حذف این آیتم مطمئن هستید؟'>
+      <button className="btn btn-secondary fw-bolder" onClick={()=> setShowModalDelete(false)}>انصراف</button>
+      <button className="btn btn-primary fw-bolder">حذف</button>
+    </Modal>
+    </>
   );
 };
 
